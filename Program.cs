3b80e51f040521e0fff8234fc9e3802ecdc78657
allenj.olsen <allenj.olsen@gmail.com>
2019-12-07 14:36:41 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace computer_vision_quickstart
+{
+    class Program
+    {
+        // Add your Computer Vision subscription key and endpoint to your environment variables. 
+        // Close/reopen your project for them to take effect.
+        //static string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
+        //static string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
+        static string subscriptionKey = "c952a46930674ff39082e88bf17cb4f1";
+        static string endpoint = "https://eastus.api.cognitive.microsoft.com/";
+        private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+        // URL image for OCR (optical character recognition). (Image of motivational meme).
+        private const string EXTRACT_TEXT_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample2.jpg";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = Authenticate(endpoint, subscriptionKey);
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+            new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+            { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
+            {
+            VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+            VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+            VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+            VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+            VisualFeatureTypes.Objects
+            };
+
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(ANALYZE_URL_IMAGE)}...");
+            Console.WriteLine();
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(ANALYZE_URL_IMAGE, features);
+
+            // Sunmarizes the image content.
+            Console.WriteLine("Summary:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+            // Display categories the image is divided into.
+            Console.WriteLine("Categories:");
+            foreach (var category in results.Categories)
+            {
+                Console.WriteLine($"{category.Name} with confidence {category.Score}");
+            }
+            Console.WriteLine();
+
+            // Image tags and their confidence score
+            Console.WriteLine("Tags:");
+            foreach (var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+
+            // Objects
+            Console.WriteLine("Objects:");
+            foreach (var obj in results.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
+                $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            // Well-known (or custom, if set) brands.
+            Console.WriteLine("Brands:");
+            foreach (var brand in results.Brands)
+            {
+                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " +
+                $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            // Adult or racy content, if any.
+            Console.WriteLine("Adult:");
+            Console.WriteLine($"Has adult content: {results.Adult.IsAdultContent} with confidence {results.Adult.AdultScore}");
+            Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
+            Console.WriteLine();
+
+            // Identifies the color scheme.
+            Console.WriteLine("Color Scheme:");
+            Console.WriteLine("Is black and white?: " + results.Color.IsBWImg);
+            Console.WriteLine("Accent color: " + results.Color.AccentColor);
+            Console.WriteLine("Dominant background color: " + results.Color.DominantColorBackground);
+            Console.WriteLine("Dominant foreground color: " + results.Color.DominantColorForeground);
+            Console.WriteLine("Dominant colors: " + string.Join(",", results.Color.DominantColors));
+            Console.WriteLine();
+
+            // Celebrities in image, if any.
+            Console.WriteLine("Celebrities:");
+            foreach (var category in results.Categories)
+            {
+                if (category.Detail?.Celebrities != null)
+                {
+                    foreach (var celeb in category.Detail.Celebrities)
+                    {
+                        Console.WriteLine($"{celeb.Name} with confidence {celeb.Confidence} at location {celeb.FaceRectangle.Left}, " +
+                        $"{celeb.FaceRectangle.Top}, {celeb.FaceRectangle.Height}, {celeb.FaceRectangle.Width}");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            // Popular landmarks in image, if any.
+            Console.WriteLine("Landmarks:");
+            foreach (var category in results.Categories)
+            {
+                if (category.Detail?.Landmarks != null)
+                {
+                    foreach (var landmark in category.Detail.Landmarks)
+                    {
+                        Console.WriteLine($"{landmark.Name} with confidence {landmark.Confidence}");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            // Detects the image types.
+            Console.WriteLine("Image Type:");
+            Console.WriteLine("Clip Art Type: " + results.ImageType.ClipArtType);
+            Console.WriteLine("Line Drawing Type: " + results.ImageType.LineDrawingType);
+            Console.WriteLine();
+
+            // Read the batch text from an image (handwriting and/or printed).
+            BatchReadFileUrl(client, EXTRACT_TEXT_URL_IMAGE).Wait();
+            //BatchReadFileLocal(client, EXTRACT_TEXT_LOCAL_IMAGE).Wait();
+            Console.ReadLine();
+        }
+
+        public static async Task BatchReadFileUrl(ComputerVisionClient client, string urlImage)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("BATCH READ FILE - URL IMAGE");
+            Console.WriteLine();
+
+            // Read text from URL
+            BatchReadFileHeaders textHeaders = await client.BatchReadFileAsync(urlImage);
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            // Retrieve the URI where the recognized text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            // Delay is between iterations and tries a maximum of 10 times.
+            int i = 0;
+            int maxRetries = 10;
+            ReadOperationResult results;
+            Console.WriteLine($"Extracting text from URL image {Path.GetFileName(urlImage)}...");
+            Console.WriteLine();
+            do
+            {
+                results = await client.GetReadOperationResultAsync(operationId);
+                Console.WriteLine("Server status: {0}, waiting {1} seconds...", results.Status, i);
+                await Task.Delay(1000);
+                if (maxRetries == 9) { Console.WriteLine("Server timed out."); }
+            }
+            while ((results.Status == TextOperationStatusCodes.Running ||
+                results.Status == TextOperationStatusCodes.NotStarted) && i++ < maxRetries);
+            // Display the found text.
+            Console.WriteLine();
+            var textRecognitionLocalFileResults = results.RecognitionResults;
+            foreach (TextRecognitionResult recResult in textRecognitionLocalFileResults)
+            {
+                foreach (Line line in recResult.Lines)
+                {
+                    Console.WriteLine(line.Text);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
